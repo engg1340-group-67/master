288255410661a7f682d117b917d21deb76cd3327
Wesely Yam <43695214+wesley34@users.noreply.github.com>
2019-04-19 02:02:51 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/*#include "add.h"
+#include "search.h"
+#include "edit.h"
+#include "delete.h"
+#include "sort.h"
+#include "login.h"
+#include "menu_kid.h"
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include<limits>
+
+using namespace std;
+
+int main() {
+	
+	string name = login_main();
+	system("cls");
+	menu_main(name);
+	return 0;
+}*/
