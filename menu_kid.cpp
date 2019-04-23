5ea9b8d44570655eb4752fd6f5c06912349f5013
Wesely Yam <43695214+wesley34@users.noreply.github.com>
2019-04-23 12:38:45 +0800

    Add files via upload

---
+++ menu_kid.cpp
@@ -0,0 +1,108 @@
+#include "edit_menu.h"
+#include "search.h"
+#include "sort.h"
+#include "add.h"
+#include "predict.h"
+#include<iostream>
+#include<fstream>
+#include<iomanip>
+#include <string>
+#include <limits>
+//#include"login.h"
+using namespace std;
+
+void our_group();
+void no_of_tasks();
+void create_task();
+//string username;
+int menu_main(string name)
+{	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	string Filename;
+	string Date;
+	char ch;
+	cout.setf(ios::fixed|ios::showpoint);
+	cout<<setprecision(2); // program outputs decimal number to two decimal places
+	our_group();
+	create_task();
+	//cout << username;
+	do
+	{	
+		system("cls");
+		cout<<"\n\n\n\tMAIN MENU";
+		cout<<"\n\n\t01. EDIT YOUR WALLET";
+	  cout<<"\n\n\t02. REPORT";
+		cout<<"\n\n\t03. SEARCH";
+		cout<<"\n\n\t04. TASKS";
+		no_of_tasks();
+		cout << "\n\n\t05. SORT";
+		cout << "\n\n\t06. INITIALIZE";
+		cout<<"\n\n\t00. EXIT";
+		cout<<"\n\n\tPlease Select Your Option (0-6) ";
+		cin>>ch;
+		switch(ch)
+		{
+			case '1':
+				cout << "\n\n\t MMDD(0101)";
+				cout << "DATE :"; cin >> Date;
+				edit_menu_main(name,Date);
+				break;
+			case '2':
+				cout << "\n\n\t MMDD(0101)";
+				cout << "DATE :"; cin >> Date;
+				predict_main(name, Date);
+				break;
+			case '3':
+				search(name);
+				break;
+			case '4':
+				break;
+			case '5':
+				cout << "\n\n\t MMDD(0101)";
+				cout << "DATE :"; cin >> Date;
+				sort(name,Date);
+				break;
+			case '6':
+				cout << "\n\n\t MMDD(0101)";
+				cout << "DATE :"; cin >> Date;
+				Filename = name+Date;
+				initialization(Filename);
+			default :cout<<"\a";
+		}
+    }while(ch!='0');
+	return 0;
+}
+
+void our_group(){
+	cout<<"\n\n\n\t  ENGG1340 Group Project";
+	cout<<"\n\n\t    Accounting system";
+	cout<<"\n\n\t Your Financial Majordomo";
+	cout<<"\n\n\n\tGroup : 67";
+	cout<<"\n\tMADE BY : Chu Wai Kit ";
+	cout<<"\n\tMADE BY : Yam Chak Fung Wesley";
+	cin.get();
+}
+void create_task(){
+	ofstream fout;
+	fout.open("task.txt");
+	if (fout.fail()){
+		cout << " Error in file opening!"<<endl;
+		exit(1);
+	}
+	fout.close();
+}
+void no_of_tasks(){
+	int n=0;
+	string line;
+	ifstream fin("task.txt");
+	if (fin.fail()){
+		cout<< " Error in file opening!"<<endl;
+		exit(1);
+	}
+	while( getline(fin,line)){
+		n++;
+	}
+	if (n >1)
+		cout <<" - "<< n << " tasks waiting you to finish";
+	else
+		cout <<" - "<< n << " task waiting you to finish";
+}
---
+++ predict.cpp
@@ -0,0 +1,211 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <stdlib.h>
+#include "delete.h"
+using namespace std;
+double reporter(string,int);//for connector use only
+vector<double> connector(string filename){
+	vector<double> predict_array;
+	double sum = 0;
+	for (int i = 0; i<6;i++){
+		predict_array.push_back(reporter(filename,i));
+	}
+	return predict_array;
+}
+vector<double> predict_mean(string name, string Date){
+	vector<vector<double>> sum; 
+	vector<double> predict_day;
+	string calander[32]={"00","01","02","03","04","05","06","07","08","09","10","11","12","13"};
+	string big_small[12] ={"31","28","31","30","31","30","31","31","30","31","30","31"};
+	for (int i = 14; i<32;i++){
+		calander[i] = to_string(i);
+	}
+	string month_s = Date.substr(0,2);
+	string filename;
+	double price = 0;
+	int day_arr[3];
+	string day_s = Date.substr(2,2);
+	int day = atoi(day_s.c_str());
+	int month = atoi(month_s.c_str());
+
+	if (day < 2){
+		if (day == 2){
+			filename = name+Date;
+			sum.push_back(connector(filename));
+			filename = name+month_s+"01";
+			sum.push_back(connector(filename));
+			filename = name+calander[month-1]+big_small[month-2];
+			sum.push_back(connector(filename));
+		}
+		else if(month == 1){
+			filename = name+Date;
+			sum.push_back(connector(filename));
+			sum.push_back(connector(filename));
+			sum.push_back(connector(filename));
+		}
+		else{
+			filename = name+Date;
+			sum.push_back(connector(filename));
+			filename = name+calander[month-1]+big_small[month-2];
+			sum.push_back(connector(filename));
+			day = atoi((big_small[month-2]).c_str());
+			filename = name+calander[month-1]+to_string(day-1);
+			sum.push_back(connector(filename));
+		}
+	}
+	else{
+	
+		filename = name+Date;
+		sum.push_back(connector(filename));
+	
+		filename = name+month_s+big_small[day-1];
+		sum.push_back(connector(filename));
+	
+		filename = name+month_s+big_small[day-2];
+		sum.push_back(connector(filename));
+	}
+
+	for (int i = 0; i<6;i++){
+		price = sum[0][i] + sum[1][i] + sum[2][i];
+		price /= 3;
+		predict_day.push_back(price);
+	}
+	return predict_day;
+}
+double reporter(string filename,int mode){
+
+	string str[6] = { "food.txt","transportation.txt","toy.txt","stationary.txt","others.txt","gains.txt" };
+	filename = filename + str[mode];
+	ifstream fvalid
+	(filename.c_str());
+	if(fvalid.fail()){
+		fvalid.close();
+		return 0;
+	}
+	fvalid.close();
+	int filesize = print_filetext(filename.c_str(),0);
+	string line;
+	double sum = 0;
+	int no,size, price;
+	// read
+	
+	ifstream fread(filename.c_str());
+	if (fread.fail()) {
+		fread.close();
+		return 0;
+	}
+	for (int i = 0; i < filesize;i++){
+		getline(fread,line);
+	}
+	fread >> line >> line >> sum;
+	fread.close();
+	//cout << "sum " << sum << endl; 
+	return sum;
+}
+
+int predict_main(string name,string Date) {
+	system("cls");
+	//storing
+	string confirm;
+	double price = 0;
+	double sum = 0;
+	vector <double> v;
+	vector <double> p = predict_mean(name,Date);
+	string filename = name + Date;
+	ofstream fout((filename + "report.txt").c_str());
+	ifstream fread((filename + "report.txt").c_str());
+	if (fread.fail()) {
+		cout << "Not such a day infomation" << endl;
+		return 0;
+	}
+	cout << "\n\n\t\t REPORT \n\n";
+	fout << "\n\n\t\t REPORT \n\n";
+	cout << "NAME: " << name << endl;
+	fout << "NAME: " << name << endl;
+	cout << "=================================================================================\n";
+	fout << "=================================================================================\n";
+	cout << "Date Food(%) Transportation(%) Toy(%) Stationary(%) Other(%) Expense(%) Income(%)" << endl;
+	fout << "Date Food(%) Transportation(%) Toy(%) Stationary(%) Other(%) Expense(%) Income(%)" << endl;
+	cout << "=================================================================================\n";
+	fout << "=================================================================================\n";
+	cout << Date << " ";
+	for (int i = 0; i < 5; i++) {
+		price = reporter(filename, i);
+		sum += price;
+		v.push_back(price);
+	}
+	v.push_back(reporter(filename, 5));
+	for (int i = 0; i < 5; i++) {
+		cout << fixed << setprecision(2) << v[i];
+		cout << "(" << v[i] / sum * 100 << "%" << ")";
+		cout << "  ";
+		fout << fixed << setprecision(2) << v[i];
+		fout << "(" << v[i] / sum * 100 << "%" << ")";
+		fout << "  ";
+	}
+	cout << fixed << setprecision << v[5];
+	cout << "(100%)";
+	fout << fixed << setprecision << v[5];
+	fout << "(100%)";
+	cout << endl;
+	fout << endl;
+	cout << "=================================================================================\n";
+	fout << "=================================================================================\n";
+	cout << "\n";
+	fout << "\n";
+	cout << "=================================================================================\n";
+	fout << "=================================================================================\n";
+	cout << "NET: "; cout << sum - v[5] << endl;
+	fout << "NET: "; fout << sum - v[5] << endl;
+	cout << "PREDICTION(NEXT DAY)\n";
+	fout << "PREDICTION(NEXT DAY)\n";
+	cout << "============================================================================\n";
+	fout << "============================================================================\n";
+	fout << "Food(%) Transportation(%) Toy(%) Stationary(%) Other(%) Expense(%) Income(%)" << endl;
+	cout << "Food(%) Transportation(%) Toy(%) Stationary(%) Other(%) Expense(%) Income(%)" << endl;
+	cout << "============================================================================\n";
+	fout << "============================================================================\n";
+	sum = 0; //reinitialize
+	price = 0;
+	for (int i = 0; i < 5; i++) {
+		price = p[i];
+		sum += price;
+		v.push_back(price);
+	}
+	for (int i = 0; i < 5; i++) {
+		cout << fixed << setprecision(2) << p[i];
+		cout << "(" << p[i] / sum * 100 << "%" << ")";
+		cout << "  ";
+		fout << fixed << setprecision(2) << p[i];
+		fout << "(" << p[i] / sum * 100 << "%" << ")";
+		fout << "  ";
+	}
+	cout << fixed << setprecision << p[5];
+	cout << "(100%)";
+	fout << fixed << setprecision << p[5];
+	fout << "(100%)";
+	cout << endl;
+	fout << endl;
+	cout << "============================================================================\n";
+	fout << "============================================================================\n";
+	cout << "\n";
+	fout << "\n";
+	cout << "============================================================================\n";
+	fout << "============================================================================\n";
+	cout << "NET: "; cout << sum - p[5] << endl;
+	fout << "NET: "; fout << sum - p[5] << endl;
+	while (1) {
+				cout << "QUIT(Y/N): "; cin >> confirm;
+				if (confirm == "Y") {
+					system("cls");
+					break;
+				}
+				else if (confirm == "N") {
+				}
+			}
+	fout.close();
+	return 0;
+}
